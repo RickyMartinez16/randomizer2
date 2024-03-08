71c939c807f9d23625ce410e536c014bb90e137e
Ricky Martinez <RMartinezW16@gmail.com>
2024-03-08 13:08:57 -0500

    comments

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Button from "./components/Button";
 import AlgoList from "./components/AlgoList";
 
 function App() {
-
+  // Initial list of algorithms
   const initialAlgorithms = ['isAnagram', 'reverse string', 'unique element'];
+
+  // State variables
   const [algorithms, setAlgorithms] = useState(initialAlgorithms);
   const [completedAlgorithms, setCompletedAlgorithms] = useState([]);
   const [currentProblem, setCurrentProblem] = useState('');
   const [newAlgorithmName, setNewAlgorithmName] = useState('');
-  
 
   // Handler for marking algorithm as completed
   const handleAlgorithmCompletion = (algorithm) => {
@@ -23,41 +24,36 @@ function App() {
     setCurrentProblem('');
   };
 
-// Handler for selecting new problem
-const handleNewProblem = () => {
-  // Filter out completed algorithms
-  const availableAlgorithms = algorithms.filter(algorithm => !completedAlgorithms.includes(algorithm));
+  // Handler for selecting new problem
+  const handleNewProblem = () => {
+    // Filter out completed algorithms
+    const availableAlgorithms = algorithms.filter(algorithm => !completedAlgorithms.includes(algorithm));
 
-  if (availableAlgorithms.length === 0) {
-    setCurrentProblem('All done!');
-  } else {
-    const randomIndex = Math.floor(Math.random() * availableAlgorithms.length);
-    setCurrentProblem(availableAlgorithms[randomIndex]);
-  }
-};
+    if (availableAlgorithms.length === 0) {
+      setCurrentProblem('All done!');
+    } else {
+      const randomIndex = Math.floor(Math.random() * availableAlgorithms.length);
+      setCurrentProblem(availableAlgorithms[randomIndex]);
+    }
+  };
 
+  // Handler for adding a new algorithm
   const handleAddAlgorithm = (algorithmName) => {
     setAlgorithms([...algorithms, algorithmName]);
   };
 
-
-  const handleAlgorithmClick = (algorithm) => {
-    // Logic to handle algorithm click
-    console.log('Clicked algorithm:', algorithm);
+  // Handler for shuffling the list of algorithms
+  const handleRandomizeAlgorithms = () => {
+    const shuffledAlgorithms = [...algorithms].sort(() => Math.random() - 0.5);
+    setAlgorithms(shuffledAlgorithms);
   };
-
-    // Handler for shuffling the list of algorithms
-    const handleRandomizeAlgorithms = () => {
-      const shuffledAlgorithms = [...algorithms].sort(() => Math.random() - 0.5);
-      setAlgorithms(shuffledAlgorithms);
-    };
 
   return (
     <div>
-    <h1>Algo Practice</h1>
-    <Button onClick={handleRandomizeAlgorithms}>Randomize Algorithms</Button>
-    <Button onClick={handleResetList}>List Reset</Button>
-    <Button onClick={handleNewProblem}>New Problem</Button>
+      <h1>Algo Practice</h1>
+      <Button onClick={handleRandomizeAlgorithms}>Randomize Algorithms</Button>
+      <Button onClick={handleResetList}>List Reset</Button>
+      <Button onClick={handleNewProblem}>New Problem</Button>
       <AlgoList 
         algorithms={algorithms} 
         onAlgorithmClick={handleAlgorithmCompletion}
@@ -71,3 +67,4 @@ const handleNewProblem = () => {
 }
 
 export default App;
+
